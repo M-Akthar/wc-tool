4a123f9f705f6b4041486645ccba1c74e5e6e80c
M-Akthar <a-zaman@live.com>
2024-07-26 12:18:28 +0100

    added single argument functionality for a valid file location

--- src/main/java/com/ma/wctool/WCToolApplicationMain.java
+++ src/main/java/com/ma/wctool/WCToolApplicationMain.java
@@ -1,14 +1,21 @@
 package com.ma.wctool;
 
-import java.io.*;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+//Some potential improvements is to accept multiple arguments, could have a boolean flag for each argument and instead of
+//the switch statement we have blocks that execute if the flag for that argument is true etc
 public class WCToolApplicationMain {
     public static void main(String[] args) throws IOException {
 //        Update as needed
 //        You can use a file to get absolute path if necessary
 //        File testFile = new File("src/main/resources/wctest2.txt");
+//        processRequest(new String[] {"-c", "src/main/resources/wctest2.txt"});
 //        processRequest(new String[] {"-l", "src/main/resources/wctest2.txt"});
+//        processRequest(new String[] {"-w", "src/main/resources/wctest2.txt"});
+//        processRequest(new String[] {"-u", "src/main/resources/wctest2.txt"});
+//        processRequest(new String[] {"src/main/resources/wctest2.txt"});
 
         if (args.length > 0) {
             try {
@@ -23,35 +30,28 @@ public class WCToolApplicationMain {
 
     private static void processRequest(String[] args) throws IOException {
         StringBuilder result = new StringBuilder();
-        BufferedReader inputFileBR;
-
-        try {
-            inputFileBR = args.length == 1 ? new BufferedReader(new FileReader(args[0])) : new BufferedReader(new FileReader(args[1]));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
         if (args.length == 1) {
-            result.append("Work in progress");
+            result.append(calculateByteCount(new BufferedReader(new FileReader(args[0])))).append(" ")
+                    .append(getLinesInFile(new BufferedReader(new FileReader(args[0])))).append(" ")
+                    .append(getWordCount(new BufferedReader(new FileReader(args[0]))));
         } else {
             switch (args[0]) {
                 case "-c":
-                    result.append(calculateByteCount(inputFileBR));
+                    result.append(calculateByteCount(new BufferedReader(new FileReader(args[1]))));
                     break;
                 case "-l":
-                    result.append(getLinesInFile(inputFileBR));
+                    result.append(getLinesInFile(new BufferedReader(new FileReader(args[1]))));
                     break;
                 case "-w":
-                    result.append(getWordCount(inputFileBR));
+                    result.append(getWordCount(new BufferedReader(new FileReader(args[1]))));
                     break;
                 default:
                     result.append("Unrecognised Command: ").append(args[0]);
-
             }
         }
 
         System.out.println(result.toString());
-        inputFileBR.close();
     }
 
     private static int calculateByteCount(BufferedReader input) throws IOException {
@@ -59,16 +59,17 @@ public class WCToolApplicationMain {
         String line;
 
         while ((line = input.readLine()) != null) {
-            //Just appending the line can result in removing the system specific line separator causing the byte count to differ
-            //compared the terminal wc -c command. Windows separator \r\n is two bytes while the unix one is a single byte
             sb.append(line).append(System.getProperty("line.separator"));
         }
 
+        input.close();
         return sb.toString().getBytes().length;
     }
 
-    private static long getLinesInFile(BufferedReader input) {
-        return input.lines().count();
+    private static long getLinesInFile(BufferedReader input) throws IOException {
+        long lineCount = input.lines().count();
+        input.close();
+        return lineCount;
     }
 
     private static int getWordCount(BufferedReader input) throws IOException {
@@ -81,6 +82,7 @@ public class WCToolApplicationMain {
             }
         }
 
+        input.close();
         return count;
     }
 }
